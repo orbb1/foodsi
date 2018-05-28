6d99532c4cee284c5650d4ff230b5da57657704b
orbb1 <orbbmax@gmail.com>
2018-05-28 22:34:05 +0200

    add charts page scss file, get charts data

--- app/pages/charts/charts.js
+++ app/pages/charts/charts.js
@@ -5,8 +5,20 @@ APP.modules = (function(modules, $) {
         template = '/pages/charts/tmpl-charts.html';
 
     modules.charts = (function() {
+        var presentData = function(data) {
+            console.log(data);
+        }
+
+        var getData = function() {
+            var httpService = APP.services.HttpService.getInstance();
+            httpService.performRequest('GET', apiUrls.API_BASE + apiUrls.INTENSITY_DATE)
+                .then(function(res) {
+                    presentData(res.data);
+                });
+        };
         var init = function() {
             $sectionContent.load(template);
+            getData();
         };
 
         var destroy = function() {
--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -1,4 +1,5 @@
 var apiUrls = {
     API_BASE: 'https://api.carbonintensity.org.uk/',
-    INTENSITY: 'intensity'
+    INTENSITY: 'intensity',
+    INTENSITY_DATE: 'intensity/date'
 };
